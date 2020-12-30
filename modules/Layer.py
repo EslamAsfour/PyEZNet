29cf7b9b5f214f748ef4d6eca741812e904321f3
EslamAsfour <40435240+EslamAsfour@users.noreply.github.com>
2020-12-30 17:37:27 +0200

    init Function done working on init_Weights

--- modules/Layer.py
+++ modules/Layer.py
@@ -32,6 +32,31 @@ class Conv2D(Layer):
         Conv2D Takes input img (Channel , Width , Height)  with N imgs -> (N , Ch , W , H)
         and Kernal Size 
         
-    
+        Inputs for the layer :
+            1-  in_Channels
+            2-  out_Channels (Number of Filter in the Conv Layer)
+            3-  Padding
+            4-  Stride
+            5-  Kernal Size (Size of the Filter ex: 3x3 filter)
     """
+    def __init__(self , in_Channels , out_Channels , Padding=0 , Stride=1 , Kernal_Size=3):
+        # Super Class will creat 
+        #    ( grad , cache , Weights , Weights_Update ) Empty local dicts
+        super().__init__()
+    
+        self.in_Channels = in_Channels
+        self.Num_Filters = out_Channels
+        self.Pad = Padding
+        self.Stride = Stride
+        self.Filter_Dim = Kernal_Size
+
+        #Based on the (Filter_Dim, in_Channels, Num_Filters) we init our weights
+        self.init_Weights()
+        
+        
+        
+        
+    def init_Weights()
+    
+    
     
