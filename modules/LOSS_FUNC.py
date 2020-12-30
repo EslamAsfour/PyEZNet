7f7f814cc0c0e55687a3181a3079cef5dd2af07b
omar-sonosy <39892158+omar-sonosy@users.noreply.github.com>
2020-12-30 23:48:08 +0200

    Add files via upload

--- modules/LOSS_FUNC.py
+++ modules/LOSS_FUNC.py
@@ -21,7 +21,7 @@ class Loss(Diff_Func):
         Returns:
         same grad value from calc_Grad()
         """
-        return self.calc_Grad (X, Y)
+        return self.grad['X']
     def calc_Grad(self, X, Y):
         """
         Local gradient with respect to X at (X, Y).
@@ -35,7 +35,9 @@ class Loss(Diff_Func):
 
 
 class Cross_Entropy_Loss(Loss):
-
+    """
+        Abstract Class to represent the Construction of the Multi class cross entropy Loss Function used for multi class classification.
+    """
 
     def forward(self, X, Y,total_loss="average"):
         #X:is np array with shape (patch,dimen)
@@ -65,7 +67,8 @@ class Cross_Entropy_Loss(Loss):
         temp_grad=probs
         for i in range (Y.size):
             temp_grad[i,Y[i]]-=1
-        grad=temp_grad
+        grad={}
+        grad['X']=temp_grad
         return grad
 
 
@@ -76,17 +79,89 @@ class Cross_Entropy_Loss(Loss):
         return (X_exp / sum_exp)
 
 
+class Hinge_Loss(Loss):
+    """
+        Abstract Class to represent the Construction of the Hinge Loss Function used for binary classification.
+    """
+    def forward(self, X, Y,total_loss="average"):
+        """
+        Computes the loss of x with respect to y.
+        Args:
+        X: numpy.ndarray of shape (n_batch,1).
+        Y: numpy.ndarray of shape (n_batch, 1).
+        Returns:
+        loss: numpy.float.
+        """
+        zeros_array=np.zeros(X.size)
+        ones_array=np.ones(X.size)
+        Loss_array=np.zeros(X.size)
+        loss_mul=np.zeros(X.size)
+        modified_mul=np.zeros(X.size)
+        for i in range (X.size):
+            loss_mul[i]=((Y[i])*(X[i]))
+            modified_mul[i]=ones_array[i]-loss_mul[i]
+        Loss_array=np.maximum(zeros_array,modified_mul)
+        self.cache['mul']=loss_mul
+        if(total_loss=="average"):
+            return np.average(Loss_array)
+        elif (total_loss=="sum"):
+            return np.sum(Loss_array)
+        elif (total_loss =="median"):
+            return np.median(Loss_array)
+
+
+    def calc_Grad(self, X, Y):
+        """
+        Computes the grad dictionary of x with respect to y.
+        Args:
+        X: numpy.ndarray of shape (n_batch,1).
+        Y: numpy.ndarray of shape (n_batch, 1).
+        Returns:
+        grad: grad dictionary.
+        """
+        grad_mul=self.cache['mul']
+        temp_grad=np.zeros(X.size)
+        for i in range (X.size):
+            if(grad_mul[i]<1):
+                temp_grad[i]=-grad_mul[i]
+            else:
+                temp_grad[i]=0
+        grad={}
+        grad['X']=temp_grad
+        return grad
 
 
 
-########################## Test case################################
 
+
+
+########################## Test case for Cross Entropy################################
+print("############################# Cross Entropy loss test ############################\n")
 list1=[[1,2,3,4,5,6],[6,2,3,4,6,7],[3,5,7,8,3,5],[1,5,8,4,5,6],[1,2,6,4,8,6]]
 list2=[5,3,5,2,1]
 X=np.array(list1)
 Y=np.array(list2)
+print("X:",X,"\n")
+print("Y:",Y,"\n")
+
 loss1=Cross_Entropy_Loss()
-foro=loss1.forward(X,Y)
-print(foro)
+foro=loss1(X,Y)
+print("Total cross entropy loss:",foro,"\n")
 back=loss1.backward()
-print(back)
+print("Gradient array of cross entropy loss:\n",back,"\n")
+
+
+
+##################################### Test case for Hinge loss ########################
+print("############################# Hinge loss test ############################\n\n")
+list2=[-5,3,-5,2,-1]
+list1=[2,5,7,9,5]
+X=np.array(list1)
+Y=np.array(list2)
+print("X:",X,"\n")
+print("Y:",Y,"\n")
+loss2=Hinge_Loss()
+forward=loss2(X,Y)
+print("Total Hinge loss:",forward,"\n")
+backward=loss2.backward()
+print("Gradient array of Hinge loss:\n",backward,"\n")
